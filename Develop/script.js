2d7c6fa36c910c6313d830712c73ae07c49f7407
Alex Messina <me@alexmessina.dev>
2023-10-24 20:34:57 -0500

    Completed JS requirements -Added a script to alter row colors -Altered script to update rows with localStorage data -Added a display of the current day with formatting

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,62 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+function updateRows(){
+  var rowArray = $("#table").children();
+  for(var index = 0; index < rowArray.length; index++){
+    var now = parseInt(dayjs().format("H"));
+    var id = rowArray[index].getAttribute("id");
+    var hour = parseInt(rowArray[index].getAttribute("id").slice(5,));
+    var currentClass = $("#"+id);
+    if(now > hour){
+      currentClass.removeClass();
+      currentClass.addClass("row time-block past");
+    }
+    else if (now == hour){
+      currentClass.removeClass();
+      currentClass.addClass("row time-block present");
+    }
+    else{
+      currentClass.removeClass();
+      currentClass.addClass("row time-block future");
+    }
+    var savedRowValues = localStorage.getItem(id);
+    var textArea = $("#"+id).children()[1]
+    textArea.value = savedRowValues;
+  }
+}
+
+function displayCurrentDay(){
+  var date = dayjs().format("dddd, MMMM D");
+  var day = parseInt(dayjs().format("D"));
+  console.log(day)
+  var daySuffix = ""
+  if(day == 1 || day == 21 || day == 31){
+    daySuffix = "st";
+  }
+  else if(day == 2 || day == 22){
+    daySuffix = "nd";
+  }
+  else if(day == 3 || day ==23){
+    daySuffix = "rd";
+  }
+  else{
+    daySuffix = "th"
+  }
+  var currentDateHTML = $("#currentDay");
+  currentDateHTML.text(date+daySuffix);
+}
+
 $(function () {
     document.addEventListener("click", function (event) {
-      var hourID = event.target.parentElement.parentElement.getAttribute("id")
-      var hourRow = $("#"+hourID).children()
-      var id = hourRow[0].getAttribute("class");
+      var hourID = event.target.parentElement.parentElement.getAttribute("id");
+      var hourRow = $("#"+hourID).children();
       var actionItems = $(hourRow[1]).val();
-      localStorage.setItem(id, actionItems)
-      var test = localStorage.getItem(id)
-      console.log(test)
+      localStorage.setItem(hourID, actionItems)     
     });
     
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+    updateRows();
+
+    displayCurrentDay();
+    
 });
